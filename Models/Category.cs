c02503a86eed70d7966cb27eafe7a5e1d6fde130
Ilijan69 <fooltiktok06@gmail.com>
2024-09-30 18:32:11 +0300

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Categories.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty; // Initialize to an empty string
+        public int DisplayOrder { get; set; }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Categories_Retry.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
